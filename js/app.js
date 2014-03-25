8c9bc5cf9bff0244a9c4560e05265fbf04936c70
Aimee Leong <aimeemayleong@gmail.com>
2014-03-25 12:25:56 +1000

    Prac 1
    Basic template in handlebars

---
+++ js/app.js
@@ -0,0 +1,119 @@
+
+
+var App = Ember.Application.create({
+  LOG_TRANSITIONS:true
+});
+
+
+App.Router.map(function() {
+  this.resource('Coffees');
+  this.resource('Coffee', {path: '/Coffees/:name'}, function() {
+    this.resource('Gallery');
+    this.resource('Reviews');
+  });
+});
+
+
+App.CoffeesRoute = Ember.Route.extend({
+  model: function(params) {
+    console.log('Params: ', params)
+    return App.COFFEE;
+  }
+});
+
+App.CoffeeRoute = Ember.Route.extend({
+  model: function(params) {
+    return App.COFFEE.findBy('name', params.name)
+  }
+});
+
+App.CoffeeIndexRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.modelFor('Coffee')
+  }
+});
+
+App.GalleryRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.modelFor('Coffee')
+  }
+});
+
+//scroll to top on route change
+App.ApplicationController = Ember.Controller.extend({
+  currentPathChanged: function () {
+    window.scrollTo(0, 0);
+  }.observes('currentPath')
+
+});
+
+// App.CoffeesController = Ember.ArrayController.extend({
+//   // queryParams: ['sort'],
+//   // sort: 'name',
+
+//   // sortByName: function() {
+//   //   var sort = this.get('sort');
+//   //   console.log(sort)
+//   // }.property('sort', 'model')
+
+//     queryParams: ['category'],
+//   category: null,
+
+//   filteredArticles: function() {
+//     var category = this.get('category');
+//     var articles = this.get('model');
+
+//     console.log(category);
+//     if (category) {
+//       return articles.filterProperty('category', category);
+//     } else {
+//       return articles;
+//     }
+//   }.property('category', 'model')
+// });
+ 
+
+
+
+$(function(){
+
+//init SwipeBox on gallery
+$('.swipebox').swipebox({
+  useCSS: true, // false will force the use of jQuery for animations</span>
+  hideBarsDelay: 0 // 0 to always show caption and action bar</span>
+});
+
+
+// function sortByProperty(property) {
+//     'use strict';
+//     return function (a, b) {
+//         var sortStatus = 0;
+//         if (a[property] < b[property]) {
+//             sortStatus = -1;
+//         } else if (a[property] > b[property]) {
+//             sortStatus = 1;
+//         }
+ 
+//         return sortStatus;
+//     };
+// }
+ 
+// $('#sortByName').on('click', function() {
+//    // console.log('got here');  
+//   var sortedByName = App.COFFEE.sort(sortByProperty('name'));
+//   console.log(sortedByName);
+//   // $("#menu").empty();
+//   // $("#menu").append(menuItemsTemplate(sortedByName));
+// });
+
+// $('#sortByPrice').on('click', function() {
+//   var sortedByPrice = coffeeItems.sort(sortByProperty('price'));
+// //   console.log(sortedByPrice);
+//   $("#menu").empty();
+//   $("#menu").append(menuItemsTemplate(sortedByPrice));
+// });
+
+}); //doc ready
+
+
+
---
+++ js/coffeeinfo.js
@@ -0,0 +1,53 @@
+
+App.COFFEE = [
+{
+  name: "Capuccino",
+  short_description: " Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  long_description: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos." ,
+  price: "$$",
+  image: "http://placehold.it/300x300",
+  who_drinks_it: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  how_to_drink: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  gallery: ["http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300"]
+}, 
+{
+  name: "Latté",
+  short_description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  long_description: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos." ,
+  price: "$$",
+  image: "http://placehold.it/300x300",
+  who_drinks_it: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  how_to_drink: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  gallery: ["http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300"]
+}, 
+{
+  name: "Flate White",
+  short_description: " Ut enim ea dolorum quam quisquam beatae doloremqun.", 
+  long_description: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos." ,
+  price: "$$",
+  image: "http://placehold.it/300x300",
+  who_drinks_it: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  how_to_drink: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  gallery: ["http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300"]
+}, 
+{
+  name: "Piccolo",
+  short_description: " Ut enim ea dolorum quam quisquam beatae doloremqun.",
+  long_description: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos." ,
+  price: "$$",
+  image: "http://placehold.it/300x300",
+  who_drinks_it: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  how_to_drink: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  gallery: ["http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300"]
+}, 
+{ 
+  name: "Machiato",
+  short_description: " Ut enim ea dolorum quam quisquam beatae doloremqun.", 
+  long_description: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos." ,
+  price: "$",
+  image: "http://placehold.it/300x300",
+  who_drinks_it: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  how_to_drink: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  gallery: ["http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300"]
+}
+  ];
