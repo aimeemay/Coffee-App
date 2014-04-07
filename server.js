1b5d117355173b75ea8100b55d04b95fd692a675
Aimee Leong <aimeemayleong@gmail.com>
2014-04-07 20:41:32 +1000

    login working

---
+++ server.js
@@ -0,0 +1,304 @@
+var coffee = [
+{
+  name: "Capuccino",
+  short_description: " Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  long_description: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos." ,
+  price: 2,
+  image: "http://placehold.it/300x300",
+  who_drinks_it: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  how_to_drink: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  gallery: ["http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300"]
+}, 
+{
+  name: "Latté",
+  short_description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  long_description: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos." ,
+  price: 2,
+  image: "http://placehold.it/300x300",
+  who_drinks_it: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  how_to_drink: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  gallery: ["http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300"]
+}, 
+{
+  name: "Flate White",
+  short_description: " Ut enim ea dolorum quam quisquam beatae doloremqun.", 
+  long_description: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos." ,
+  price: 2,
+  image: "http://placehold.it/300x300",
+  who_drinks_it: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  how_to_drink: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  gallery: ["http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300"]
+}, 
+{
+  name: "Piccolo",
+  short_description: " Ut enim ea dolorum quam quisquam beatae doloremqun.",
+  long_description: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos." ,
+  price: 2,
+  image: "http://placehold.it/300x300",
+  who_drinks_it: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  how_to_drink: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  gallery: ["http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300"]
+}, 
+{ 
+  name: "Machiato",
+  short_description: " Ut enim ea dolorum quam quisquam beatae doloremqun.", 
+  long_description: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos." ,
+  price: 1,
+  image: "http://placehold.it/300x300",
+  who_drinks_it: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  how_to_drink: "Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos. Velit reiciendis voluptas assumenda doloremque temporibus nisi quos.",
+  gallery: ["http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300", "http://placehold.it/300x300"]
+}
+  ];
+
+
+ // Look at the ember: http://jsbin.com/izijal/9/edit
+
+var express = require('express')
+  , passport = require('passport')
+  , flash = require('connect-flash')
+  , LocalStrategy = require('passport-local').Strategy;
+
+
+var users = [
+    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
+  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+];
+
+function findById(id, fn) {
+  var idx = id - 1;
+  if (users[idx]) {
+    fn(null, users[idx]);
+  } else {
+    fn(new Error('User ' + id + ' does not exist'));
+  }
+}
+
+function findByUsername(username, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.username === username) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
+
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+
+// Use the LocalStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a username and password), and invoke a callback
+//   with a user object.  In the real world, this would query a database;
+//   however, in this example we are using a baked-in set of users.
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+
+      // Find the user by username.  If there is no user with the given
+      // username, or the password is not correct, set the user to `false` to
+      // indicate failure and set a flash message.  Otherwise, return the
+      // authenticated `user`.
+      findByUsername(username, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+        return done(null, user);
+      })
+    });
+  }
+));
+
+
+var app = express();
+
+// configure Express
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.engine('ejs', require('ejs-locals'));
+  app.use(express.logger());
+  app.use(express.cookieParser('some secret'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.session({
+    secret: 'keyboard cat',
+    cookie: {
+      expires : new Date(Date.now() + 36000) //1 Hour
+    }
+  }));
+  app.use(flash());
+  // Initialize Passport!  Also use passport.session() middleware, to support
+  // persistent login sessions (recommended).
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+
+// app.get('/', function(req, res){ //isn't this the same as 'index' - aimee
+//   // if (req.session){
+//   ///  console.log("Session Time Left", req.session.cookie['_expires'])
+//   //  console.log("Session Time Left", req.user)
+
+//   //res.render('index', { user: req.user, session: req.session.cookie['_expires'] }); //what is the 2nd argument doing here? - aimee
+//   //what if wanted a route that didn't require authentication but if it did, should display user's name and logout button? - aimee
+// });
+
+// app.get('/account', ensureAuthenticated, function(req, res){
+//   res.render('account', { user: req.user });
+// });
+
+// app.get('/login', function(req, res){
+//   res.render('login', { user: req.user, message: req.flash('error') }); //why isn't their a ensureAuth here? - aimee
+// });
+
+// POST /login
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+//
+//   curl -v -d "username=bob&password=secret" http://127.0.0.1:3000/login
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/', failureFlash: true }),
+  function(req, res) {
+    res.redirect('/#/Coffees');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.listen(4242, function() {
+  console.log('Express server listening on port 4242');
+});
+
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); } //what is passed into req? the user? - aimee
+  res.redirect('/login')
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////// 			     	 OLD			   ////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// var users = [{
+//   username: "paul",
+//   password: "pass"
+// },
+// {
+//   username: "aimee",
+//   password: "pass"
+// }];
+
+// var application_root = __dirname,
+//     express = require("express"),
+//     path = require("path"),
+//     passport = require("passport"),
+//     flash = require('connect-flash'),
+//     LocalStrategy = require("passport-local").Strategy;
+
+// var app = express();
+
+// // Config
+// app.configure(function () {
+//   // app.use(express.logger('dev'));
+//   app.use(express.static(path.join(application_root, "public")));
+//   app.use(express.cookieParser());
+//   app.use(express.bodyParser());
+//   // app.use(express.methodOverride());
+//   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+//   app.use(passport.initialize());
+//   app.use(passport.session()); // persistent login sessions
+//   // app.use(flash());
+//   app.use(app.router);
+// });
+
+// user validation 
+// passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//   	console.log('username: ' + username + ", password: " + password)
+//    	var user = (username === 'a')    
+//    	var pass = (password === 'b')
+
+//       if (!user) {
+//         return done(null, false, { message: 'Incorrect username.' });
+//       }
+//       if (!password) {
+//         return done(null, false, { message: 'Incorrect password.' });
+//       }
+//       return done(null, user);
+//   }
+// ));
+
+// app.post('/haha',
+//   passport.authenticate('local', { successRedirect: '/#/Coffees',
+//                                    failureRedirect: '/',
+//                                    failureFlash: true })
+// );
+
+//////////////Paul's Authentication//////////////
+//User validation
+// var auth = express.basicAuth(function(user, pass) {     
+//    return (user == "super" && pass == "secret");
+// },'Super duper secret area');
+
+// var HTTPauth = express.basicAuth(function(user, pass, callback) {
+//  var result = (user === 'a' && pass === 'b');
+//  callback(null /* error */, result);
+// });
+
+// function auth(req, res, next) {
+// 	var a = HTTPauth(req, res, next);
+// }
+// //Password protected area
+// app.get('/', HTTPauth, function(req, res){
+// 	res.send('test')
+// });
+//////////////Paul's Authentication//////////////
+
+//////////////NEXT PRAC//////////////
+// app.get('/api', function (req, res) {
+//   coffee_list = coffee.map(function(x){
+//   	return { name: x.name, short_description: x.short_description, price: x.price};
+//   })
+//   res.send(coffee_list);
+// });
+
+// app.get("/api/:name", function(req, res){
+// 	res.send(coffee.filter(function(a) {
+// 		return req.params.name === a.name;
+// 	}))
+// })
+//////////////NEXT PRAC//////////////
+
+
+
+// Launch server
+// app.listen(4242);
